cd8c66c7c39584a617c2b20eafa27acdb721a18f
Joshua Sharp <joshuasfsharp@gmail.com>
2023-11-15 00:32:53 +1100

    pass db client to product manager routes

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -2,10 +2,12 @@ import { Elysia } from "elysia";
 import { createClient } from "edgedb";
 import { User } from "@project-aegis/db/dbschema/interfaces";
 import e from "@project-aegis/db/dbschema/edgeql-js";
-import { productManager } from "@project-aegis/product-manager";
+import { getProductManagerRoutes } from "@project-aegis/product-manager";
 
 const dbClient = createClient();
 
-export const app = new Elysia().decorate("db", dbClient).use(productManager);
+export const app = new Elysia().decorate("db", dbClient);
+
+app.use(getProductManagerRoutes(app));
 
 app.listen(3000);
